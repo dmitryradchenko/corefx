b4f54afd8f6ae9e46d644f382df1eb1d488c075b
Levi Broderick <levib@microsoft.com>
2019-02-21 13:25:11 -0800

    Add more Utf8String ref APIs

--- src/System.Utf8String/ref/System.Utf8String.cs
+++ src/System.Utf8String/ref/System.Utf8String.cs
@@ -7,13 +7,20 @@
 
 namespace System
 {
-    public sealed partial class Utf8String
+    public sealed partial class Utf8String : IEquatable<Utf8String>
     {
         public static readonly Utf8String Empty;
         public Utf8String(ReadOnlySpan<byte> value) { }
         public Utf8String(ReadOnlySpan<char> value) { }
+        public static bool operator ==(Utf8String a, Utf8String b) => throw null;
+        public static bool operator !=(Utf8String a, Utf8String b) => throw null;
         public int Length => throw null;
         [ComponentModel.EditorBrowsable(ComponentModel.EditorBrowsableState.Never)] // for compiler use only
+        public override bool Equals(object obj) => throw null;
+        public bool Equals(Utf8String value) => throw null;
+        public static bool Equals(Utf8String a, Utf8String b) => throw null;
+        public override int GetHashCode() => throw null;
         public ref readonly byte GetPinnableReference() => throw null;
+        public override string ToString() => throw null;
     }
 }
